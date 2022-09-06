50746fd03f0593073758d11d4f439058533bc870
dupree719 <isaiahdup7@gmail.com>
2022-09-06 01:37:49 -0400

    My third commit

---
+++ src/main/java/Pokemon.java
@@ -0,0 +1,19 @@
+public class Pokemon {
+
+    String name;
+    int level;
+
+    Pokemon(){
+        level = 1;
+    }
+
+    Pokemon(String name, int level){
+        this.name = name;
+        this.level = level;
+    }
+
+    void attack(){
+        System.out.println(name + " Attack!");
+
+    }
+}
---
+++ src/main/java/TutorialMain.java
@@ -0,0 +1,18 @@
+public class TutorialMain {
+    public static void main(String[] args) {
+
+        // class_name var = new class_name()
+        Pokemon p1 = new Pokemon("Isaiah", 719);
+        System.out.println(p1.level);
+        p1.attack();
+
+//        p1.name = "Isaiah";
+//        p1.level = 719;
+//
+//        Pokemon p2 = new Pokemon();
+//        p2.name = "JarJar";
+//        p2.level = 8
+
+        //p2.attack();
+    }
+}
